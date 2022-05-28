9870b4d363b343ab3a8e7737e5178d8a6a7443f2
KevinBaoB <kevinbaobui@gmail.com>
2022-05-28 13:18:05 -0500

    First

--- js/calculateMode.js
+++ js/calculateMode.js
@@ -1,3 +1,30 @@
-const calculateMode = () => {
+const calculateMode = (arr) => {
+  const obj = {};
+  let res = [];
+  arr.forEach((num) => {
+    if (!obj[num]) {
+      obj[num] = 1;
+    } else {
+      obj[num] += 1;
+    }
+  });
+  let highestVal = 0;
+  let highestKey = -Infinity;
 
-}
+  for (let key in obj) {
+    const value = obj[key];
+    if (value > highestVal) {
+      highestVal = value;
+      highestKey = key;
+    }
+    if (value === highestVal) {
+      res.push(key);
+    }
+  }
+  // console.log(obj);
+  // console.log(res);
+  return res;
+};
+
+console.log(calculateMode([3, 5, 4, 4, 1, 1, 2, 3]));
+// console.log(calculateMode(["who", "what", "where", "who"]));
