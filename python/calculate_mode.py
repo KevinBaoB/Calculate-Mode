c85b0e049252672efc9e31a85f450042cca936ed
KevinBaoB <kevinbaobui@gmail.com>
2022-05-29 22:02:59 -0500

    Translated

--- python/calculate_mode.py
+++ python/calculate_mode.py
@@ -1,2 +1,10 @@
-def calculate_mode():
-    pass
+def calculate_mode(arr):
+    obj = {}
+    for i in arr:
+        if not i in obj:
+            obj[i] = 1
+        else:
+            obj[i] += 1
+    return [j for j, k in obj.items() if k == max(obj.values())]
+
+print(calculate_mode([3, 5, 4, 4, 1, 1, 2, 3]))
